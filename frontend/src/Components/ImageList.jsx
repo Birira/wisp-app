59fc0ce3c74e2bf61fc5d6806e0ef32314300302
Vicente Pereira <vicente.pereira.ch@gmail.com>
2024-08-14 00:08:46 -0400

    tamaño bien

--- frontend/src/Components/ImageList.jsx
+++ frontend/src/Components/ImageList.jsx
@@ -4,7 +4,7 @@ const bookService = new BookService();
 
 const books = await bookService.getBooks();
 
-
+console.log(books);
 const ImageList = () => {
 
   const [Libros, setLibros] = useState([]);
@@ -13,14 +13,15 @@ const ImageList = () => {
     setLibros(books)
   }, []);
 
+
   return (
-    <div className='container d-flex align-items-center'>
+    <div className='container align-items-center'>
       <div className='row m-0'>
         {Libros.map((libro) => (
-          <div key={libro._id} className="col-3">
-            <figure className='figure text-center p-4'>
+          <div key={libro._id} className="col-3 p-3">
+            <figure className='figure text-center'>
               <img src={`https://test-railway-pr-production.up.railway.app/${libro.image}`} width={300}
-                height={300} className="rounded p-1" />
+                height={300} className="rounded" />
               <figcaption className='figure-caption'>{libro.title}</figcaption>
             </figure>
           </div>
